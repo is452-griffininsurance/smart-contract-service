c4c56865f4b8525d0b5810680dd60663b36605aa
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-29 21:26:24 +0800

    feat: using truffle now

---
+++ README.md
@@ -0,0 +1,21 @@
+# is452-js-backend
+
+
+## To Run
+
+To install,
+
+```sh
+npm i
+```
+
+Next up, go to `.env.example` and make a copy as `.env`, then insert your Ethereum wallet's private key here:
+```
+WALLET_PRIVATE_KEY=
+```
+
+To compile the contracts, you just need to run
+
+```sh
+truffle compile
+```
--- sc_middleware_functions/web3-solc-contract-compile-deploy.js
+++ sc_middleware_functions/web3-solc-contract-compile-deploy.js
@@ -1,66 +1,39 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+require('dotenv').config()
 const Web3 = require("web3");
+const SmartInsurance = require("../blockchain/abis/SmartInsurance.json")
+
 var adminBalance = undefined;
-const adminWalletAddress = "0x515fB466aFDec3cD3982dce918E5c289dB0f0255"
+const adminWalletAddress = "0xfeB87197aBd18dDaBD28B58b205936dfB4569B17" // Our wallet
 
 // Connect to Ethereum node
-const web3_conn = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/3f9b178d47664bbcb9b3a3f542a39d6e"));
+const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/3f9b178d47664bbcb9b3a3f542a39d6e"));
 // console.log(Object.keys(web3_conn))
 // console.log(Object.keys(web3_conn.eth))
 // console.log(web3_conn)
-web3_conn.eth.getBalance(adminWalletAddress, async (err, result) => {
+web3.eth.getBalance(adminWalletAddress, async (err, result) => {
     if (err) {
         console.log(err);
         return;
     }
-    adminBalance = web3_conn.utils.fromWei(result, "ether");
+    adminBalance = web3.utils.fromWei(result, "ether");
 })
 
 
-
-// Compile the source code 
-const safeMathPath = path.resolve(__dirname, 'contracts', 'SafeMath.sol');
-const safeMathContractFile = fs.readFileSync(safeMathPath, 'UTF-8');
-const smartInsurancePath = path.resolve(__dirname, 'contracts', 'SmartInsurance.sol');
-const smartInsuranceContractFile = fs.readFileSync(smartInsurancePath, 'UTF-8');
-
-const contracts = {
-    language: "Solidity",
-    sources: {
-        'SafeMath.sol': {
-            content: safeMathContractFile
-        },
-        'SmartInsurance.sol': {
-            content: smartInsuranceContractFile
-        }
-    },
-    settings: {
-        outputSelection: {
-            "*": {
-                "*": [ '*' ]
-            }
-        }
-    }
-}
-
-const output = JSON.parse(solc.compile(JSON.stringify(contracts)))
-const bytecode = output.contracts['SmartInsurance.sol']["SmartInsurance"].evm.bytecode.object;
-const abi = output.contracts['SmartInsurance.sol']["SmartInsurance"].abi;
+const account = web3.eth.accounts.privateKeyToAccount(process.env.WALLET_PRIVATE_KEY);
 
 // Creating contract object
-const smartInsurance = new web3_conn.eth.Contract(abi);
+const smartInsurance = new web3.eth.Contract(SmartInsurance.abi);
 
 // Function parameter
 let payload = {
-    data: bytecode
+    data: SmartInsurance.bytecode,
+    arguments: ["0xfeB87197aBd18dDaBD28B58b205936dfB4569B17", "SQ306", "2020-11-30"]
 }
 
 let parameter = {
-    from: adminWalletAddress,
-    gas: web3_conn.utils.toHex(80000),
-    gasPrice: web3_conn.utils.toHex(web3_conn.utils.toWei("30", "gwei"))
+    from: web3.eth.accounts[0],
+    gas: web3.utils.toHex(80000),
+    gasPrice: web3.utils.toHex(web3.utils.toWei("30", "gwei"))
 }
 
 smartInsurance.deploy(payload).send(parameter, (err, transactionHash) => {
@@ -69,11 +42,11 @@ smartInsurance.deploy(payload).send(parameter, (err, transactionHash) => {
     console.log('Deployed Contract Address: ', newContractInstance.options.address);
 })
 
-exports.test_sc = function(req, res) {
-    console.log(adminBalance);
-    res.json({"message": "success"})
-}
+// exports.test_sc = function(req, res) {
+//     console.log(adminBalance);
+//     res.json({"message": "success"})
+// }
 
-exports.create_account = function(req, res) {
-    res.json({"message":"creating account"})
-}
+// exports.create_account = function(req, res) {
+//     res.json({"message":"creating account"})
+// }
